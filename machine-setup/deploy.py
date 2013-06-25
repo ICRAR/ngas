e3285e81e6acb0586a9275387fd0d5a737f03513
awicenec <andreas.wicenec@icrar.org>
2013-06-25 13:33:42 +0800

    Fixed copy of NGAS DB template into test directory
    Former-commit-id: 425a2de3cbee184bb0ece00581a889a954b3ebba
    Former-commit-id: 45d0446013d185ea77582074cad2efa2fb349334

--- machine-setup/deploy.py
+++ machine-setup/deploy.py
@@ -570,7 +570,7 @@ def ngas_buildout():
         with settings(warn_only=True):
             run('sqlite3 -init {0}/src/ngamsSql/ngamsCreateTables-SQLite.sql ngas.sqlite <<< $(echo ".quit")'\
                 .format(env.NGAS_DIR_ABS))
-            run('cp ngas.sqlite {0}/src/ngamsTest/src/ngas_Sqlite_db_template'.format(NGAS_DIR_ABS))
+            run('cp ngas.sqlite {0}/src/ngamsTest/src/ngas_Sqlite_db_template'.format(env.NGAS_DIR_ABS))
 
 
 @task
