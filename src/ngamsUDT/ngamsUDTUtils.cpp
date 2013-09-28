b89ee4dbb179c412f4badb5eca3437754b7f3c3f
Dave Pallot <dave.pallot@curtin.edu.au>
2013-09-28 20:50:15 +0800

    added ngamsUDTUtils
    Former-commit-id: db7ae340e9f7107f3c5f3c8f48b09ba38386309f
    Former-commit-id: 83fc039e5bd8924ab8aa3d9688859ab74ab3106d

---
+++ src/ngamsUDT/ngamsUDTUtils.cpp
@@ -0,0 +1,272 @@
+/*
+ * ngamsUDTUtils.cpp
+ *
+ *  Created on: Sep 28, 2013
+ *      Author: dpallot
+ */
+
+#include <netdb.h>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <inttypes.h>
+#include <errno.h>
+
+#include "ngamsUDTUtils.h"
+#include "udt.h"
+
+#define MAXLINE 16384
+#define MAXELEMENTS 100
+
+int readline(int fd, string& line, unsigned int maxlen, int (*recvfunc)(int, char*, int)) {
+	char c;
+	int rc;
+
+	line.clear();
+
+	while (true) {
+		rc = recvfunc(fd, &c, 1);
+		if (rc <= 0)
+			return -1;
+
+		if (c =='\n')
+			return line.size();
+
+		line += c;
+
+		if (line.size() == maxlen)
+			return maxlen;
+	}
+}
+
+
+int HTTPHeaderToString(const HTTPHeader* hdr, string& str) {
+	str.clear();
+	str.append(hdr->status);
+	str.append("\015\012");
+
+	map<string, string>::const_iterator iter;
+	for (iter = (hdr->vals).begin(); iter != (hdr->vals).end(); iter++) {
+		str.append(iter->first);
+		str.append(":");
+		str.append(iter->second);
+		str.append("\015\012");
+	}
+
+	str.append("\015\012");
+
+	return 0;
+}
+
+
+int readHTTPHeader(int fd, HTTPHeader* hdr, int (*recvfunc)(int, char*, int)) {
+
+	string line;
+	bool first = true;
+
+	int read = 0;
+	while (true) {
+		read = readline(fd, line, MAXLINE, recvfunc);
+		if (read == MAXLINE) {
+			cout << "max line reached" << endl;
+			return -1;
+		}
+
+		// Check if max number of HTTP lines reached
+		if (hdr->vals.size() >= MAXELEMENTS) {
+			cout << "max number of http elements reached" << endl;
+			return -1;
+		}
+
+		// the end of http header
+		if (line.size() == 0) {
+			//cout << "end of header" << endl;
+			return 0;
+		}
+
+		// end of line but there is a single carridge return
+		// http header ends with an empty line i.e /r/n
+		if (line.size() == 1 and line[0] == '\015') {
+			//cout << "end of header" << endl;
+			return 0;
+		}
+
+		// read the status http header
+		if (first) {
+			hdr->status = line;
+			//cout << hdr->status << endl;
+			first = false;
+		}
+		else {
+			//split string into key:value
+			size_t found = line.find(":");
+			if (found != std::string::npos) {
+				string key = line.substr(0, found);
+				string value = line.substr(found+1, line.size());
+				if (key.size() > 0) {
+					// convert key to lowercase
+					transform(key.begin(), key.end(), key.begin(), ::tolower);
+					// store key:value pair in a collection map
+					hdr->vals[key] = value;
+					//cout << key << " " << value << endl;
+				}
+			}
+		} //end else
+	} //end while
+
+	return 0;
+}
+
+int writeHTTPPacket(int fd, const HTTPHeader* hdr, const HTTPPayload* payload, int (*writefunc)(int, const char*, int))
+{
+	string hdrStr;
+	HTTPHeaderToString(hdr, hdrStr);
+
+	// write header
+	int ret = writefunc(fd, hdrStr.c_str(), hdrStr.size());
+	if (ret <= 0)
+		return -1;
+
+	// write payload
+	ret = writefunc(fd, payload->buff, payload->payloadsize);
+	if (ret <= 0)
+		return -1;
+
+	return 0;
+}
+
+int readHTTPPacket(int fd, HTTPHeader* hdr, HTTPPayload* payload, int (*recvfunc)(int, char*, int))
+{
+	// read the NGAS response header
+	int ret = readHTTPHeader(fd, hdr, recvfunc);
+	if (ret < 0) {
+	   cout << "invalid HTTP header" << endl;
+	   return -1;
+	}
+
+	// get the payload size from the HTTP header
+	string content("content-length");
+	if (hdr->vals.find(content) == hdr->vals.end()) {
+	   cout << "content-length in HTTP header does not exist" << endl;
+	   return -1;
+	}
+
+	char* endptr;
+	// convert filesize from string to int64
+	int64_t contentsize = strtoumax(hdr->vals[content].c_str(), &endptr, 10);
+	if (contentsize == 0 || errno == ERANGE) {
+		cout << "error parsing content-length" << endl;
+		return -1;
+	}
+
+	payload->payloadsize = contentsize;
+	payload->buff = new char[contentsize];
+	if (payload->buff == NULL)
+		return -1;
+
+	ret = recvfunc(fd, payload->buff, contentsize);
+	if (ret <= 0) {
+		cout << "error reading http payload" << endl;
+		delete[] payload->buff;
+		payload->buff = NULL;
+		return -1;
+	}
+
+	return 0;
+}
+
+
+int connect(const char* host, const int port) {
+	struct hostent* hostRef;
+	struct sockaddr_in servAddr;
+
+	if ((hostRef = gethostbyname(host)) == NULL)
+		return -1;
+
+	memset((char *)&servAddr, 0, sizeof(servAddr));
+	memcpy(&servAddr.sin_addr, hostRef->h_addr_list[0], hostRef->h_length);
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_port = htons(port);
+
+	int fd = socket(PF_INET, SOCK_STREAM, 0);
+	if (fd < 0)
+		return -1;
+
+	int ret = connect(fd, (struct sockaddr*)&servAddr, sizeof(struct sockaddr_in));
+	if (ret < 0) {
+		close(fd);
+		return -1;
+	}
+
+	return fd;
+}
+
+
+int reliableTCPWrite(int fd, const char* buf, int len) {
+	int towrite = len;
+	int written = 0;
+	int ret = 0;
+
+	while (written < towrite) {
+		ret = write(fd, buf+written, towrite);
+		if (ret <= 0)
+			return ret;
+
+		written += ret;
+		towrite -= ret;
+	}
+
+	return len;
+}
+
+int reliableUDTWrite(int u, const char* buf, int len) {
+	int towrite = len;
+	int written = 0;
+	int ret = 0;
+
+	while (written < towrite) {
+		ret = UDT::send(u, buf+written, towrite, 0);
+		if (ret == UDT::ERROR)
+			return UDT::ERROR;
+
+		written += ret;
+		towrite -= ret;
+	}
+
+	return len;
+
+}
+
+int reliableUDTRecv(int u, char* buf, int len) {
+	int read = 0;
+	int toread = len;
+	int ret = 0;
+
+	while (read < toread) {
+		ret = UDT::recv(u, buf+read, toread, 0);
+		if (ret == UDT::ERROR)
+			return UDT::ERROR;
+
+		read += ret;
+		toread -= ret;
+	}
+
+	return len;
+}
+
+int reliableTCPRecv(int u, char* buf, int len) {
+	int read = 0;
+	int toread = len;
+	int ret = 0;
+
+	while (read < toread) {
+		ret = recv(u, buf+read, toread, 0);
+		if (ret <= 0)
+			return ret;
+
+		read += ret;
+		toread -= ret;
+	}
+
+	return len;
+}
