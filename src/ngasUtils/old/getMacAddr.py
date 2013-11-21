65b71f318bf873ccbbd35fe1f32eb7a0423041e8
awicenec <andreas.wicenec@icrar.org>
2013-11-21 13:10:05 +0800

    new option dio (DIRECT_IO) added
    Former-commit-id: 2f525e87d152bbc7abe6d86e0c8f323a90c4e807
    Former-commit-id: c15f296242be3436ec5a0311e2f90509672f78eb

--- src/ngasUtils/old/getMacAddr.py
+++ src/ngasUtils/old/getMacAddr.py
@@ -17,23 +17,23 @@ def getMacAddr(interface):
         result = fcntl.ioctl(s.fileno(), IN.SIOCGIFHWADDR, interface + \
 		'\0'*256)
     except IOError,e:
-	print 'IO ERROR occured: ',e
-	s.close()
-	return 'xx:xx:xx:xx'
+    print 'IO ERROR occured: ',e
+    s.close()
+    return 'xx:xx:xx:xx'
     except:
-	print 'Unspecified error occured: ',sys.exc_info()[1] 
-	s.close()
-	return 'xx:xx:xx:xx'
+    print 'Unspecified error occured: ',sys.exc_info()[1]
+    s.close()
+    return 'xx:xx:xx:xx'
     s.close()
 
     H_MACADDR = result[18:24]
     MACADDR = ''
 
     for c in range(len(H_MACADDR)):
-	lc = hex(struct.unpack('B',H_MACADDR[c])[0])[2:].upper()
-	if len(lc) == 1:
-	   lc = '0' + lc
-	MACADDR = MACADDR + lc + ':'
+    lc = hex(struct.unpack('B',H_MACADDR[c])[0])[2:].upper()
+    if len(lc) == 1:
+        lc = '0' + lc
+    MACADDR = MACADDR + lc + ':'
 
     return MACADDR[0:-1]
 
