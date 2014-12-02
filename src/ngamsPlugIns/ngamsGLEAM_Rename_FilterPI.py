9756e30a04dedae36c29126ac3bc704bb6506ab0
Chen Wu <chen.wu@icrar.org>
2014-12-02 17:35:30 +0800

    rename phase 2 measurement set files
    Former-commit-id: 37572b84060b559aa96b6c8eaeb4a6d205d6ea70
    Former-commit-id: a941cb42b526ed9cadf0ec9b6a4792695b1fbcd5

---
+++ src/ngamsPlugIns/ngamsGLEAM_Rename_FilterPI.py
@@ -0,0 +1,70 @@
+#
+#    (c) University of Western Australia
+#    International Centre of Radio Astronomy Research
+#    M468/35 Stirling Hwy
+#    Perth WA 6009
+#    Australia
+#
+#    Copyright by UWA,
+#    All rights reserved
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#    MA 02111-1307  USA
+#
+#******************************************************************************
+# Who       When        What
+# --------  ----------  -------------------------------------------------------
+# cwu      02/Dec/2014  Created
+
+"""
+Pick only measuremenetset tar ball (e.g. 1069415952.tar.gz)
+"""
+import os
+
+from ngams import *
+
+def ngamsGLEAM_Rename_FilterPI(srvObj,
+                          plugInPars,
+                          filename,
+                          fileId,
+                          fileVersion = -1,
+                          reqPropsObj = None):
+    
+    """
+    srvObj:        Reference to NG/AMS Server Object (ngamsServer).
+
+    plugInPars:    Parameters to take into account for the plug-in
+                   execution (string).
+   
+    fileId:        File ID for file to test (string).
+
+    filename:      Filename of (complete) (string).
+
+    fileVersion:   Version of file to test (integer).
+
+    reqPropsObj:   NG/AMS request properties object (ngamsReqProps).
+ 
+    Returns:       0 if the file does not match, 1 if it matches the
+                   conditions (integer/0|1).
+    """
+    ss = fileId.split('.')
+    if (len(ss) != 3):
+        return 0
+    if (ss[1] != 'tar' or ss[2] != 'gz'):
+        return 0
+    if (len(ss[0]) != 10):
+        return 0
+    
+    return 1
