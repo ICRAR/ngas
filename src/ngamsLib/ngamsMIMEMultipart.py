a8c87b82539efe019d8366202e11408374e092f8
Rodrigo Tobar <rodrigo.tobar@uwa.edu.au>
2015-05-27 12:42:35 +0800

    Removed erroneous import

--- src/ngamsLib/ngamsMIMEMultipart.py
+++ src/ngamsLib/ngamsMIMEMultipart.py
@@ -19,7 +19,6 @@
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 #    MA 02111-1307  USA
 #
-from ngamsFileInfo import ngamsFileInfo
 """
 Module containing classes to deal with MIME multipart
 messages representing the contents of an
