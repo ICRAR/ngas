61c81d9ba0e610cedb7bcbf82032a6bc4077fb43
Rodrigo Tobar <rtobar@icrar.org>
2015-06-18 16:36:01 +0800

    egg-ifying ngamsTest and fixing ngamsCClient compilation

--- src/ngamsCore/setup.py
+++ src/ngamsCore/setup.py
@@ -19,7 +19,6 @@
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 #    MA 02111-1307  USA
 #
-import os
 from setuptools import setup, find_packages
 
 with open('../../VERSION') as vfile:
--- src/ngamsTest/setup.py
+++ src/ngamsTest/setup.py
@@ -1,26 +1,54 @@
+#
+#    ICRAR - International Centre for Radio Astronomy Research
+#    (c) UWA - The University of Western Australia, 2015
+#    Copyright by UWA (in the framework of the ICRAR)
+#    All rights reserved
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#    MA 02111-1307  USA
+#
 from setuptools import setup, find_packages
-import sys, os
 
-version = '0.0'
+with open('../../VERSION') as vfile:
+    for line in vfile.readlines():
+        if "ngamsNGAMS_SW_VER" in line:
+            version = line.split("NGAMS_SW_VER ")[1].strip()[1:-1]
+            break
 
-setup(name='ngamsTest',
-      version=version,
-      description="",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='',
-      author_email='',
-      url='',
-      license='',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=True,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+setup(
+    name='ngamsTest',
+    version=version,
+    description="NGAMs test modules",
+    long_description="The modules that contain NGAMS' tests",
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    packages=find_packages(),
+    include_package_data=True,
+    package_data = {
+        'ngamsTest' : ['README', 'TODO', 'src/*', 'ref/*']
+    },
+    # No spaces allowed between the '='s
+    entry_points= {
+        'console_scripts':[
+            'ngamsTest=ngamsTest.ngamsTest:main'
+        ],
+    },
+    install_requires=[
+        'pcc',
+        'ngamsCore',
+        'ngamsPlugIns',
+        'ngamsServer',
+        'ngamsPClient'
+    ]
+)
