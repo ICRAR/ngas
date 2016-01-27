7745f3e85fa90ce13db07800ea2b0e6f529fcca5
Rodrigo Tobar <rtobar@icrar.org>
2016-01-27 16:25:31 +0800

    Removing sqlite from the NGAS build

--- build.sh
+++ build.sh
@@ -5,30 +5,30 @@ fail() {
 	exit 1
 }
 
+# Check that we're using a virtualenv
 if test -z "$VIRTUAL_ENV"
 then
 	fail "No VIRTUAL_ENV variable defined, make sure you have the correct environment loaded"
 fi
 
+# Make sure we're standing where alongside this script
+# in order to properly execute the rest of the stuff
+this=$0
+if [ -h $0 ]
+then
+	this=$(readlink -f $0)
+fi
+cd $(dirname $this)
+
+# And now...
 cd src
 
-# Create the configure script for the ngamsCClient module
+# Build the C autotools-based module
 cd ngamsCClient
 ./bootstrap || fail "Failed to bootstrap ngamsCClient module"
+./configure --prefix="$VIRTUAL_ENV" || fail "Failed to ./configure $cModule"
+make clean all install || fail "Failed to compile $cModule"
 cd ..
-
-# Uncompress sqlite from its sources
-tar xf ../clib_tars/sqlite-autoconf-3070400.tar.gz || fail "Failed to uncompress the sqlite module"
-
-# Build autotools-based modules
-for cModule in sqlite-autoconf-3070400 ngamsCClient
-do
-	prevDir=$(pwd -P)
-	cd $cModule
-	./configure --prefix="$VIRTUAL_ENV" || fail "Failed to ./configure $cModule"
-	make clean all install || fail "Failed to compile $cModule"
-	cd "$prevDir"
-done
 
 # Build python setup.py-based modules
 for pyModule in pcc ngamsCore ngamsPClient ngamsServer ngamsPlugIns
