7156bffa372c4db23c36a70bd4581bf53fe8a030
davepallot <dave.pallot@uwa.edu.au>
2016-06-01 15:49:09 +0800

    Don't rely on os.rename as it can cause issues across disk boundaries

--- src/ngamsCore/ngamsLib/ngamsCore.py
+++ src/ngamsCore/ngamsLib/ngamsCore.py
@@ -1062,12 +1062,10 @@ def mvFile(srcFilename,
         checkAvailDiskSpace(trgFilename, fileSize)
         timer = PccUtTime.Timer()
 
-        # Don't rely on executing separate OS commands to move files
-        # Do it the python way instead
-        os.rename(srcFilename, trgFilename)
+        # Don't rely on os.rename as it can cause issues when crossing 
+        # disk parition boundaries
+        shutil.move(srcFilename, trgFilename)
 
-        # os.rename returns nothiing but throws OSErrors
-        #if (stat): raise Exception, "Error executing move command: " + str(out)
         deltaTime = timer.stop()
     except Exception, e:
         errMsg = genLog("NGAMS_AL_MV_FILE", [srcFilename, trgFilename, str(e)])
