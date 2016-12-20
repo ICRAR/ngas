5dc8662af578f9d4e5db063f48183df9f0fbfce8
Rodrigo Tobar <rtobar@icrar.org>
2016-12-20 11:17:18 +0800

    New from/to iso8601/mjd functions
    The from/to iso8601 functions are meant to replace the several existing string
    <--> time functions throughout the code, primarily those under PccUtTime (with
    the purpose of finally removing that module), and unify them. They work both
    with UTC (default) and local time conversions and offer different formatting
    options, and thus should accommodate for most of the current use cases.
    
    The from/to mjd functions are meant to replace the functionality found inside
    the PccUtTime class at the moment, but without the need of constructing a class
    around the calculation, thus making it more straightforward.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- src/ngamsCore/ngamsLib/ngamsCore.py
+++ src/ngamsCore/ngamsLib/ngamsCore.py
@@ -58,6 +58,8 @@ For more detailed information about the project, consult the URL:
   http://www.eso.org/projects/ngas
 """
 
+import calendar
+import collections
 import glob
 import importlib
 import logging
@@ -816,8 +818,7 @@ def timeRef2Iso8601(timeRef):
             timeRefConv = timeRef
     elif (type(timeRef) in (types.IntType, types.FloatType)):
         # Assume seconds from epoch.
-        timeRefConv = PccUtTime.TimeStamp().initFromSecsSinceEpoch(timeRef).\
-                      getTimeStamp()
+        timeRefConv = toiso8601(timeRef, local=True)
     else:
         # Assume DateTime object.
         timeRefConv = PccUtTime.TimeStamp().\
@@ -1026,4 +1027,75 @@ def execCmd(cmd, timeOut = -1, shell=True):
             raise Exception('Command %s timed out after %.2f [s]' % (cmd, time.time() - startTime))
 
     return p.poll(), stdout, stderr
-# EOF
+
+
+_formatspec = collections.namedtuple('_formatspec', 'format msecs')
+FMT_DATE_ONLY        = _formatspec('%Y-%m-%d',          False)
+FMT_TIME_ONLY        = _formatspec('%H:%M:%S',          True)
+FMT_TIME_ONLY_NOMSEC = _formatspec('%H:%M:%S',          False)
+FMT_DATETIME         = _formatspec('%Y-%m-%dT%H:%M:%S', True)
+
+def fromiso8601(s, local=False, fmt=FMT_DATETIME):
+    """
+    Converts string `s` to the number of seconds since the epoch,
+    as returned by time.time.
+
+    `s` should be expressed in the ISO 8601 extended format indicated by the
+    `fmt` flag.
+
+    If `local` is False then `s` is assumed to represent time at UTC,
+    otherwise it is interpreted as local time. In either case `s` should contain
+    neither time-zone information nor the 'Z' suffix.
+    """
+
+    ms = 0.
+    if fmt.msecs:
+        s, ms = s.split(".")
+        ms = float(ms)/1000.
+
+    totime = calendar.timegm if not local else time.mktime
+    t = totime(time.strptime(s, fmt.format))
+    return t + ms
+
+def toiso8601(t=None, local=False, fmt=FMT_DATETIME):
+    """
+    Converts the time value `t` to a string formatted using the ISO 8601
+    extended format indicated by the `fmt` flag.
+
+    `t` is expressed in numbers of seconds since the epoch, as returned by
+    time.time. If `t` is not given, the current time is used instead.
+
+    If `local` is False then the resulting string represents the time at UTC.
+    If `local` is True then the resulting string represents the local time.
+    In either case the string contains neither time-zone information nor the 'Z'
+    suffix.
+    """
+
+    if t is None:
+        t = time.time()
+
+    totuple = time.gmtime if not local else time.localtime
+    timeStamp = time.strftime(fmt.format, totuple(t))
+    if fmt.msecs:
+        t = (t - long(t)) * 1000
+        timeStamp += '.%03d' % int(t)
+
+    return timeStamp
+
+_UNIX_EPOCH_AS_MJD = 40587.
+def tomjd(t=None):
+    """
+    Returns the Modified Julian Date for the given Unix time.
+    """
+    if t is None:
+        t = time.time()
+    return t/86400. + _UNIX_EPOCH_AS_MJD
+
+def frommjd(mjd):
+    """
+    Returns the Unix time for the given Modified Julian Date.
+
+    This algorithm is suitable at least for dates after the Unix Epoch.
+    """
+    # 40587 is the MJD for the Unix Epoch
+    return (mjd - _UNIX_EPOCH_AS_MJD)*86400.
