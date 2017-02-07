676fe78ddb253f859fd04012112b5ce0a47d452d
Rodrigo Tobar <rtobar@icrar.org>
2017-02-07 12:33:14 +0800

    Explicit imports
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- src/ngamsTest/ngamsTest/ngamsPawseyStageTest.py
+++ src/ngamsTest/ngamsTest/ngamsPawseyStageTest.py
@@ -20,11 +20,13 @@
 #    MA 02111-1307  USA
 #
 
-import sys
 import os
 import socket
+import sys
 import ngamsTestLib
-from ngamsPlugIns.ngamsMWAPawseyTapeApi import *
+
+from ngamsPlugIns.ngamsMWAPawseyTapeApi import isFileOffline, readDMFStatus,\
+    pawseyMWAdmget, releaseFiles
 
 os.environ['PATH'] += ':' + os.path.join(os.path.dirname(__file__)) + '/bin'
 
