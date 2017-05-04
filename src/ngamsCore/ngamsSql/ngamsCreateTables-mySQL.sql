cabbd8ef2b7f355c2d5f49bb02f3603a8a41495d
Rodrigo Tobar <rtobar@icrar.org>
2017-05-04 18:27:22 +0700

    Updating MySQL creation script
    All DROPs are first, following by the creation of the tables. All table
    descriptions have been updated to contain all new columns and tables.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- src/ngamsCore/ngamsSql/ngamsCreateTables-mySQL.sql
+++ src/ngamsCore/ngamsSql/ngamsCreateTables-mySQL.sql
@@ -1,8 +1,18 @@
-use ngas;
-
-
 drop table IF EXISTS ngas_cache;
-Create table ngas_cache
+drop table IF EXISTS ngas_files;
+drop table IF EXISTS ngas_containers;
+drop table IF EXISTS ngas_cfg;
+drop table IF EXISTS ngas_cfg_pars;
+drop table IF EXISTS ngas_disks;
+drop table IF EXISTS ngas_disks_hist;
+drop table IF EXISTS ngas_hosts;
+drop table IF EXISTS ngas_mirroring_queue;
+drop table IF EXISTS ngas_mirroring_hist;
+drop table IF EXISTS ngas_subscribers;
+drop table IF EXISTS ngas_subscr_back_log;
+drop table IF EXISTS ngas_subscr_queue;
+
+create table ngas_cache
 (
     disk_id       varchar(128)    not null,
     file_id       varchar(64)     not null,
@@ -12,8 +22,6 @@ Create table ngas_cache
     constraint ngas_cache_idx primary key(disk_id, file_id, file_version)
 );
 
-
-drop table IF EXISTS ngas_cfg;
 create table ngas_cfg (
   cfg_name           varchar(32)  not null,
   cfg_par_group_ids  text         not null,
@@ -22,7 +30,6 @@ create table ngas_cfg (
 );
 
 
-drop table IF EXISTS ngas_cfg_pars;
 create table ngas_cfg_pars
 (
   cfg_group_id       varchar(32)  not null,
@@ -32,8 +39,6 @@ create table ngas_cfg_pars
   constraint         cfg_group_idx primary key(cfg_group_id, cfg_par, cfg_val)
 );
 
-
-drop table IF EXISTS ngas_disks;
 create table ngas_disks
 (
   disk_id                 varchar(128)   not null,
@@ -41,7 +46,7 @@ create table ngas_disks
   installation_date       varchar(23)    not null,
   type                    varchar(64)    not null,
   manufacturer            varchar(64)    null,
-  capacity_mb     	      numeric(20,0)  not null,
+  capacity_mb             numeric(20,0)  default 0,
   logical_name            varchar(128)   not null,
   host_id                 varchar(32)    null,
   slot_id                 varchar(32)    null,
@@ -59,8 +64,6 @@ create table ngas_disks
   constraint disk_idx primary key(disk_id)
 );
 
-
-drop table IF EXISTS ngas_disks_hist;
 create table ngas_disks_hist
 (
   disk_id                 varchar(128)   not null,
@@ -74,8 +77,20 @@ create index ngas_disks_hist_disk_id  on ngas_disks_hist(disk_id);
 create index ngas_disks_hist_date     on ngas_disks_hist(hist_date);
 create index ngas_disks_hist_origin   on ngas_disks_hist(hist_origin);
 
+CREATE TABLE ngas_containers
+(
+  container_id        VARCHAR(36)    NOT NULL,
+  parent_container_id VARCHAR(36)    NULL,
+  container_name      VARCHAR(255)   NOT NULL,
+  ingestion_date      VARCHAR(23)    NULL,
+  container_size      NUMERIC(20, 0) NOT NULL,
+  container_type      VARCHAR(10)    NOT NULL,
+  CONSTRAINT container_idx PRIMARY KEY(container_id),
+  CONSTRAINT container_uni UNIQUE(parent_container_id, container_name),
+  CONSTRAINT container_parent FOREIGN KEY (parent_container_id) REFERENCES ngas_containers(container_id)
+);
 
-drop table IF EXISTS ngas_files;
+
 create table ngas_files
 (
   disk_id                varchar(128)   not null,
@@ -93,12 +108,13 @@ create table ngas_files
   file_status            char(8)        default '00000000',
   creation_date          varchar(23)    null,
   io_time                numeric(20, 0) default -1,
+  ingestion_rate         numeric(20, 0) NULL DEFAULT -1,
+  container_id           varchar(36)    NULL,
   constraint file_idx primary key(file_id,file_version,disk_id)
 );
+ALTER TABLE ngas_files ADD CONSTRAINT file_container FOREIGN KEY (container_id) REFERENCES ngas_containers(container_id);
 
-
-drop table IF EXISTS ngas_hosts;
-Create table ngas_hosts
+create table ngas_hosts
 (
   host_id              varchar(32)    not null,
   domain               varchar(30)    not null,
@@ -107,8 +123,8 @@ Create table ngas_hosts
   n_slots              smallint       null,
   cluster_name         varchar(16)    null,
   installation_date    varchar(23)    null,
-  srv_version          varchar(20)    null,
-  srv_port             int            null,
+  srv_version          varchar(40)    null,
+  srv_port             int            not null,
   srv_archive          smallint       null,
   srv_retrieve         smallint       null,
   srv_process          smallint       null,
@@ -126,11 +142,9 @@ Create table ngas_hosts
   srv_suspended        smallint       null,
   srv_req_wake_up_srv  varchar(32)    null,
   srv_req_wake_up_time varchar(23)    null,
-  constraint host_idx primary key(host_id,srv_port)
+  constraint host_idx primary key(host_id, srv_port)
 );
 
-
-drop table IF EXISTS ngas_mirroring_queue;
 Create table ngas_mirroring_queue
 (
   instance_id           varchar(32)     not null,
@@ -146,9 +160,7 @@ Create table ngas_mirroring_queue
   constraint ngas_mirroring_queue_idx primary key(file_id, file_version)
 );
 
-
-drop table IF EXISTS ngas_mirroring_hist;
-Create table ngas_mirroring_hist
+create table ngas_mirroring_hist
 (
   instance_id           varchar(32)     not null,
   file_id               varchar(64)     not null,
@@ -163,9 +175,7 @@ Create table ngas_mirroring_hist
   constraint ngas_mirroring_hist_idx primary key(file_id, file_version)
 );
 
-
-drop table IF EXISTS ngas_subscribers;
-Create table ngas_subscribers
+create table ngas_subscribers
 (
   host_id                   varchar(32)   not null,
   srv_port                  int           not null,
@@ -175,16 +185,13 @@ Create table ngas_subscribers
   subscr_start_date         varchar(23)   null,
   subscr_filter_plugin      varchar(64)   null,
   subscr_filter_plugin_pars varchar(128)  null,
-  last_file_ingestion_date varchar(23) null
+  last_file_ingestion_date  varchar(23)   null,
+  concurrent_threads	    int           default 1 null
 );
-
 create unique index subscr_id_idx on ngas_subscribers(subscr_id);
-
 create unique index host_id_srv_port_idx on ngas_subscribers(host_id, srv_port);
 
-
-drop table IF EXISTS ngas_subscr_back_log;
-Create table ngas_subscr_back_log
+create table ngas_subscr_back_log
 (
   host_id        varchar(32)   not null,
   srv_port       int           not null,
@@ -197,3 +204,18 @@ Create table ngas_subscr_back_log
   format         varchar(32)   not null
 );
 
+create table ngas_subscr_queue
+(
+  subscr_id         varchar(255)  not null,
+  file_id           varchar(64)   not null,
+  file_version      int           default 1,
+  disk_id           varchar(128)  not null,
+  file_name         varchar(255)  not null,
+  ingestion_date    varchar(23)   not null,
+  format            varchar(32)   not null,
+  status            int           default -2,
+  status_date       varchar(23)   not null,
+  comment           varchar(255)  null,
+  constraint subscr_queue_idx primary key(subscr_id,file_id,file_version,disk_id)
+);
+create index subscr_queue_subscr_id_idx on ngas_subscr_queue(subscr_id);
