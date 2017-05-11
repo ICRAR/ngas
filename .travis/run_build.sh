29c49c5513b72d1c0c0b8c4611e43452f6c8ff09
Rodrigo Tobar <rtobar@icrar.org>
2017-05-11 16:51:21 +0800

    Correct password assignment
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- .travis/run_build.sh
+++ .travis/run_build.sh
@@ -52,7 +52,7 @@ if [[ "$DB" == "mysql" ]]; then
 	# Create ngas database, we keep using the "travis" user
 	# (who might have already all priviledges over its newly created database)
 	mysql_cmd="mysql -uroot -e"
-	$mysql_cmd "CREATE USER 'ngas'@'%' IDENTIFIED BY PASSWORD('ngas');" || fail "$EUSER"
+	$mysql_cmd "CREATE USER 'ngas'@'%' IDENTIFIED BY 'ngas';" || fail "$EUSER"
 	$mysql_cmd "CREATE DATABASE ngas;" || fail "$EDB"
 	$mysql_cmd "GRANT ALL ON ngas.* TO 'ngas'@'%';" || fail "$EPERM"
 
