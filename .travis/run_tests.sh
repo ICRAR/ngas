1d3f5080bdedf183faa9852d65d6179677cd4c3a
Rodrigo Tobar <rtobar@icrar.org>
2017-05-11 14:38:10 +0800

    First try to run tests against different databases
    The .travis.yml file now specifies three different items in the matrix build,
    each one dedicated to a different database engine. The heavy lifting of
    preparing the database and setting the appropriate environment variables for
    the tests to run against a given database resides in the run_*.sh scripts under
    the new .travis directory; this way we don't clutter the .travis.yml file
    *that* much.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

---
+++ .travis/run_tests.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# NGAS test runner script to Travis CI
+#
+# ICRAR - International Centre for Radio Astronomy Research
+# (c) UWA - The University of Western Australia, 2016
+# Copyright by UWA (in the framework of the ICRAR)
+# All rights reserved
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307  USA
+
+# We know the following variables:
+#
+# DB                the database used in this run (sqlite3, mysql, postgresql)
+# TRAVIS_BUILD_DIR  the directory where the sources are located
+#
+
+NGAS_TESTDB=
+
+# These are the user/dbname/passwd that we created on run_build
+if [[ "$DB" == "mysql" ]]; then
+	NGAS_TESTDB='<Db Id="blah" Snapshot="1" Interface="MySQLdb" host="localhost" db="ngas" user="ngas" passwd="ngas"/>'
+elif [[ "$DB" == "postgresql" ]]; then
+	NGAS_TESTDB='<Db Id="blah" Snapshot="1" Interface="psycopg2" host="localhost" dbname="ngas" user="ngas" password="ngas"/>'
+fi
+# sqlite3 is the default so it needs no special attention
+
+cd ${TRAVIS_BUILD_DIR}/src/ngamsTest/ngamsTest
+NGAS_TESTDB=${NGAS_TESTDB} python ngamsTest.py
