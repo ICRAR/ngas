dcee0765b91a16b2a17ca8b89e0ade4a8958d276
Rodrigo Tobar <rtobar@icrar.org>
2017-11-16 13:51:06 +0800

    Re-implementing SUBSCRIBE command
    The command simply adds a new subscriber to the database, and its
    corresponding retroactive deliveries. For good measure, it also kicks in
    the subscription thread to start working as soon as possible.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- src/ngamsServer/ngamsServer/ngamsSubscribeCmd.py
+++ src/ngamsServer/ngamsServer/ngamsSubscribeCmd.py
@@ -34,35 +34,14 @@ This module contains functions used in connection with the SUBSCRIBE Command.
 import logging
 import time
 
-from ngamsLib.ngamsCore import \
-    genLog, NGAMS_SUBSCRIBE_CMD, TRACE, fromiso8601, toiso8601
+import ngamsSubscriptionThread
+from ngamsLib.ngamsCore import fromiso8601, toiso8601
 from ngamsLib import ngamsSubscriber, ngamsLib
 
 
 logger = logging.getLogger(__name__)
 
-def addSubscriber(srvObj, subscrObj):
-    """
-    Add a Subscriber to the list of Subscribers. The information about
-    the Subscriber is also updated in the DB.
-
-    srvObj:      Reference to NG/AMS Server object (ngamsServer).
-
-    subscrObj:   Subscriber Object (ngamsSubscriber).
-
-    Returns:     Void.
-    """
-    T = TRACE()
-
-    srvObj.getDb().insertSubscriberEntry(subscrObj)
-    #subscrObj.write(srvObj.getDb())
-
-    srvObj.registerSubscriber(subscrObj)
-
-
-def handleCmd(srvObj,
-                       reqPropsObj,
-                       httpRef):
+def handleCmd(srv, reqPropsObj, httpRef):
     """
     Handle SUBSCRIBE Command.
 
@@ -76,68 +55,57 @@ def handleCmd(srvObj,
 
     Returns:        Void.
     """
-    T = TRACE()
 
-    """
-    if (srvObj.getDataMoverOnlyActive() and len(srvObj.getSubscriberDic()) > 0):
-        srvObj.reply(reqPropsObj, httpRef, NGAMS_HTTP_SUCCESS, NGAMS_FAILURE,
-                 "Data Mover NGAS server can have only one subscriber. Use command USUBSCRIBE to update an existing subscriber.")
-        return
-    """
-    priority      = 10
-    url           = ""
-    startDate     = time.time()
-    filterPi      = ""
-    filterPiPars  = ""
-    if (reqPropsObj.hasHttpPar("priority")):
-        priority = reqPropsObj.getHttpPar("priority")
-    if (reqPropsObj.hasHttpPar("url")):
-        url = reqPropsObj.getHttpPar("url")
-        ngamsSubscriber.validate_url(url)
-    else:
-        errMsg = genLog("NGAMS_ER_CMD_SYNTAX",
-                        [NGAMS_SUBSCRIBE_CMD, "Missing parameter: url"])
-        raise Exception, errMsg
-    if (reqPropsObj.hasHttpPar("start_date")):
+    # URL is mandatory, and has some requirements
+    if 'url' not in reqPropsObj:
+        raise Exception('Missing parameter: url')
+    url = reqPropsObj['url']
+    ngamsSubscriber.validate_url(url)
+
+    # Concurrent threads must be >= 1
+    concur_thrds  = 1
+    if 'concurrent_threads' in reqPropsObj:
+        concur_thrds = int(reqPropsObj['concurrent_threads'])
+    if concur_thrds < 1:
+        raise Exception('Number of concurrent threads is < 1')
+
+    # Priority must be an integer
+    priority = 10
+    if 'priority' in reqPropsObj:
+        priority = int(reqPropsObj['priority'])
+
+    # start_date must be in ISO 8601 format
+    startDate = time.time()
+    if 'start_date' in reqPropsObj:
         tmpStartDate = reqPropsObj.getHttpPar("start_date").strip()
         if tmpStartDate:
             startDate = fromiso8601(tmpStartDate, local=True)
-    if (reqPropsObj.hasHttpPar("filter_plug_in")):
-        filterPi = reqPropsObj.getHttpPar("filter_plug_in")
-    if (reqPropsObj.hasHttpPar("plug_in_pars")):
-        filterPiPars = reqPropsObj.getHttpPar("plug_in_pars")
-    if (reqPropsObj.hasHttpPar("subscr_id")):
-        id = reqPropsObj.getHttpPar("subscr_id")
+
+    # Filter Plug-in + pars
+    filterPi      = None
+    filterPiPars  = None
+    if 'filter_plug_in' in reqPropsObj:
+        filterPi = reqPropsObj['filter_plug_in']
+    if 'plug_in_pars' in reqPropsObj:
+        filterPiPars = reqPropsObj['plug_in_pars']
+
+    # Subscriber ID. If not given, we use the URL
+    if 'subscr_id' in reqPropsObj:
+        subscr_id = reqPropsObj['subscr_id']
     else:
-        id = ngamsLib.getSubscriberId(url)
+        subscr_id = ngamsLib.getSubscriberId(url)
 
     logger.info("Creating subscription for files >= %s", toiso8601(startDate))
-    subscrObj = ngamsSubscriber.ngamsSubscriber(srvObj.getHostId(),
-                                                srvObj.getCfg().getPortNo(),
-                                                priority, url, startDate,
-                                                filterPi, filterPiPars, subscrId=id)
-    # supports concurrent file transfer, added by chen.wu@icrar.org
-    if (reqPropsObj.hasHttpPar("concurrent_threads")):
-        concurthrds = reqPropsObj.getHttpPar("concurrent_threads")
-        subscrObj.setConcurrentThreads(concurthrds)
+    args = (subscr_id, srv.getHostId(), srv.portNo, priority,
+            url, startDate, concur_thrds, True, filterPi, filterPiPars)
+    subscriber = ngamsSubscriber.ngamsSubscriber(*args)
 
-    # If the Start Date given in before the Last Ingestion Date, we
-    # reset the Last Ingestion Date
-    subscrStat = srvObj.getDb().getSubscriberStatus([subscrObj.getId()],
-                                                    subscrObj.getHostId(),
-                                                    subscrObj.getPortNo())
-    if subscrStat:
-        lastIngDate = subscrStat[0][1]
-        if startDate < lastIngDate:
-            subscrObj.setLastFileIngDate(None)
-        else:
-            subscrObj.setLastFileIngDate(lastIngDate)
+    # Register the new subscriber and its deliveries
+    srv.db.insertSubscriberEntry(subscriber)
+    ngamsSubscriptionThread.add_deliveries_for_new_subscriber(srv, subscriber)
 
-    # Register the Subscriber.
-    addSubscriber(srvObj, subscrObj)
-
-    # Trigger the Data Susbcription Thread to make it check if there are
-    # files to deliver to the new Subscriber.
-    srvObj.addSubscriptionInfo([], [subscrObj]).triggerSubscriptionThread()
+    # Trigger the Data Susbcription Thread to see the effects of this addition
+    # soon
+    srv.triggerSubscriptionThread()
 
     return "Handled SUBSCRIBE command"
