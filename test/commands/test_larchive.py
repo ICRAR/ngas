22e5e84ae1c46d819553fb3d9b79a29a3d782704
Rodrigo Tobar <rtobar@icrar.org>
2018-10-30 16:08:00 +0800

    Unit-testing the LARCHIVE command
    We are starting with a very very simple test, but at least it gives us
    the confidence that the "happy path" of the code is working as expected.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

---
+++ test/commands/test_larchive.py
@@ -0,0 +1,39 @@
+#
+#    ICRAR - International Centre for Radio Astronomy Research
+#    (c) UWA - The University of Western Australia, 2018
+#    Copyright by UWA (in the framework of the ICRAR)
+#    All rights reserved
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#    MA 02111-1307  USA
+#
+from ngamsLib.ngamsCore import cpFile
+
+from ..ngamsTestLib import ngamsTestSuite, tmp_path
+
+class LarchiveTest(ngamsTestSuite):
+
+    def test_simple(self):
+
+        _, db = self.prepExtSrv()
+        client = self.get_client()
+        copy_of_copy = tmp_path('cp_cp')
+        cpFile('/bin/cp', copy_of_copy)
+        pars = [['fileUri', copy_of_copy]]
+        self.assert_ngas_status(client.archive, 'file:' + copy_of_copy, 'application/octet-stream', pars=pars, cmd='LARCHIVE')
+
+        # Check that it exists in the database (duplicated) and that it can be retrieved
+        self.assertEqual(2, db.query2('SELECT count(*) FROM ngas_files')[0][0])
+        self.assert_ngas_status(client.retrieve, 'cp_cp', targetFile=tmp_path())
