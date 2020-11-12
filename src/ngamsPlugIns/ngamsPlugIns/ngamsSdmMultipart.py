c4f6c03267d3e79ffc3648e9358f5fcae89ff1b7
Stewart McLay <smclay@eso.org>
2020-11-12 21:59:32 +0100

    Apply python 2/3 workaround for reading binary message from file

--- src/ngamsPlugIns/ngamsPlugIns/ngamsSdmMultipart.py
+++ src/ngamsPlugIns/ngamsPlugIns/ngamsSdmMultipart.py
@@ -42,13 +42,13 @@ import email
 import logging
 import os
 import re
+import sys
 
 from ngamsLib import ngamsCore, ngamsPlugInApi
 from ngamsLib.ngamsCore import genLog
 
-# _EXT = '.msg'
-_PLUGIN_ID = __name__
-logger = logging.getLogger(__name__)
+# EXT = '.msg'
+PLUGIN_ID = __name__
 
 # This matches the old UID of type X0123456789abcdef/X01234567
 # UID_EXPRESSION = re.compile("[xX][0-9,a-f]{3,}/[xX][0-9,a-f]{1,}$")
@@ -58,6 +58,13 @@ logger = logging.getLogger(__name__)
 
 # Update for the new assignment of archiveIds (backwards compatible)
 UID_EXPRESSION = re.compile(r"^[uU][iI][dD]://[aAbBcCzZxX][0-9,a-z,A-Z]+(/[xX][0-9,a-z,A-Z]+){2}(#\w{1,}|/\w{0,}){0,}$")
+
+# Python 2/3 workaround
+message_from_file = email.message_from_file
+if sys.version_info[0] > 2:
+    message_from_file = email.message_from_binary_file
+
+logger = logging.getLogger(__name__)
 
 
 def specific_treatment(file_path):
@@ -73,11 +80,11 @@ def specific_treatment(file_path):
     filename = os.path.basename(file_path)
     try:
         with open(file_path, 'rb') as file_object:
-            message = email.message_from_file(file_object)
+            message = message_from_file(file_object)
             file_type = message.get_content_type()
     except Exception as ex:
         error = "Parsing of mime message failed: " + str(ex)
-        error_message = genLog("NGAMS_ER_DAPI_BAD_FILE", [filename, _PLUGIN_ID, error])
+        error_message = genLog("NGAMS_ER_DAPI_BAD_FILE", [filename, PLUGIN_ID, error])
         raise Exception(error_message)
 
     # First we try looking up the 'alma-uid' header
@@ -90,12 +97,12 @@ def specific_treatment(file_path):
     # Otherwise we throw an exception
     if alma_uid is None:
         error = "Mandatory alma-uid or Content-Location parameter not found in mime header!"
-        error_message = genLog("NGAMS_ER_DAPI_BAD_FILE", [filename, _PLUGIN_ID, error])
+        error_message = genLog("NGAMS_ER_DAPI_BAD_FILE", [filename, PLUGIN_ID, error])
         raise Exception(error_message)
 
     if not UID_EXPRESSION.match(alma_uid):
         error = "Invalid alma-uid found in Content-Location: " + str(alma_uid)
-        error_message = genLog("NGAMS_ER_DAPI_BAD_FILE", [filename, _PLUGIN_ID, error])
+        error_message = genLog("NGAMS_ER_DAPI_BAD_FILE", [filename, PLUGIN_ID, error])
         raise Exception(error_message)
 
     # Now, build final filename. We do that by looking for the UID in
@@ -114,11 +121,11 @@ def specific_treatment(file_path):
         final_filename = alma_uid.replace('/', ':')
 
         # Have no idea why, but the extension is added somewhere else
-        # if os.path.splitext(final_filename)[-1] != _EXT:
-        #     final_filename += _EXT
+        # if os.path.splitext(final_filename)[-1] != EXT:
+        #     final_filename += EXT
     except Exception as ex:
         error = "Problem constructing final file name: " + str(ex)
-        error_message = genLog("NGAMS_ER_DAPI_BAD_FILE", [filename, _PLUGIN_ID, error])
+        error_message = genLog("NGAMS_ER_DAPI_BAD_FILE", [filename, PLUGIN_ID, error])
         raise Exception(error_message)
 
     return file_id, final_filename, file_type
@@ -201,5 +208,5 @@ def ngamsSdmMultipart(server_object, request_object):
     except Exception as ex:
         error_message = genLog("NGAMS_ER_DAPI_BAD_FILE",
                                [os.path.basename(staging_filename),
-                                _PLUGIN_ID, str(ex)])
+                                PLUGIN_ID, str(ex)])
         raise Exception(error_message)
